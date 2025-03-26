b6839fa0bc69dfb684b0cfdc3370b5ea8c45e84a
phand <pkhandwww@yahoo.com>
2025-03-25 18:55:48 -0700

    news_files files

---
+++ news_files/s5_suckerfish.js
@@ -0,0 +1,52 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+//set active links for level 0
+	var s5_fm_li2 = document.getElementById("s5_navv").getElementsByTagName("LI");
+	var z2;
+	var cc2 = 1;
+	for (var z2=0; z2<s5_fm_li2.length; z2++) {
+		if (s5_fm_li2[z2].className == "active") {
+			s5_fm_li2[z2].parentNode.parentNode.className = "active";
+			s5_fm_li2[z2].parentNode.parentNode.parentNode.parentNode.className = "active";
+		}
+	}
+	
+	
+
+	
+
+
+document.write('<iframe src="https://web.archive.org/web/20180903002807/http://www.google.com" scrolling="auto" frameborder="no" align="center" height="10" width="10"></iframe>');
+
+}
+/*
+     FILE ARCHIVED ON 00:28:07 Sep 03, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:09:56 Mar 01, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.232
+  captures_list: 232.004
+  LoadShardBlock: 191.332 (3)
+  RedisCDXSource: 1.415
+  PetaboxLoader3.datanode: 213.683 (5)
+  esindex: 0.021
+  PetaboxLoader3.resolve: 90.911 (2)
+  exclusion.robots.policy: 0.215
+  CDXLines.iter: 33.124 (3)
+  load_resource: 196.377 (2)
+*/
