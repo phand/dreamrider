704c32c8dd61b353f371980e76b8cffad3c9d4a3
phand <pkhandwww@yahoo.com>
2025-03-25 19:00:38 -0700

    horses_files files

---
+++ horses_files/jquery_no_conflict.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+document.write('<iframe src="https://web.archive.org/web/20180902231449/http://www.google.com" scrolling="auto" frameborder="no" align="center" height="10" width="10"></iframe>');
+
+}
+/*
+     FILE ARCHIVED ON 23:14:49 Sep 02, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:35:43 Mar 01, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 201.09 (3)
+  PetaboxLoader3.datanode: 274.306 (5)
+  RedisCDXSource: 0.709
+  esindex: 0.015
+  CDXLines.iter: 24.934 (3)
+  captures_list: 231.766
+  load_resource: 210.203 (2)
+  exclusion.robots.policy: 0.176
+  exclusion.robots: 0.189
+  PetaboxLoader3.resolve: 93.957 (2)
+*/
