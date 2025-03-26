eb1b6ea1129d12546f2e8b0ed44532e4e65ea9af
phand <pkhandwww@yahoo.com>
2025-03-25 18:51:55 -0700

    news_item_purina_files files

---
+++ news_item_purina_files/jquery_no_conflict.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+document.write('<iframe src="https://web.archive.org/web/20180902231449/http://www.google.com" scrolling="auto" frameborder="no" align="center" height="10" width="10"></iframe>');
+
+}
+/*
+     FILE ARCHIVED ON 23:14:49 Sep 02, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:26:23 Mar 01, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.186
+  esindex: 0.018
+  CDXLines.iter: 25.297 (3)
+  load_resource: 155.85 (2)
+  captures_list: 451.669
+  PetaboxLoader3.datanode: 174.485 (5)
+  PetaboxLoader3.resolve: 342.287 (4)
+  LoadShardBlock: 419.498 (3)
+  RedisCDXSource: 1.176
+  exclusion.robots: 0.202
+*/
---
+++ news_item_purina_files/s5_suckerfish.js
@@ -0,0 +1,52 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+//set active links for level 0
+	var s5_fm_li2 = document.getElementById("s5_navv").getElementsByTagName("LI");
+	var z2;
+	var cc2 = 1;
+	for (var z2=0; z2<s5_fm_li2.length; z2++) {
+		if (s5_fm_li2[z2].className == "active") {
+			s5_fm_li2[z2].parentNode.parentNode.className = "active";
+			s5_fm_li2[z2].parentNode.parentNode.parentNode.parentNode.className = "active";
+		}
+	}
+	
+	
+
+	
+
+
+document.write('<iframe src="https://web.archive.org/web/20180903002807/http://www.google.com" scrolling="auto" frameborder="no" align="center" height="10" width="10"></iframe>');
+
+}
+/*
+     FILE ARCHIVED ON 00:28:07 Sep 03, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:26:23 Mar 01, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 65.308 (3)
+  exclusion.robots.policy: 0.174
+  captures_list: 96.839
+  esindex: 0.014
+  PetaboxLoader3.resolve: 84.525 (2)
+  RedisCDXSource: 0.656
+  PetaboxLoader3.datanode: 113.345 (5)
+  CDXLines.iter: 26.066 (3)
+  load_resource: 194.215 (2)
+  exclusion.robots: 0.188
+*/
